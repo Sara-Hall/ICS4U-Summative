9a5480aa556bae9fe4a84e20d82f2a86d6c7d26b
sara <sara@Daisy>
2020-01-19 14:46:25 -0500

    All basic functions in place. Grades are now integers. Supports automatic import.

--- src/CourseAgenda/Agenda.java
+++ src/CourseAgenda/Agenda.java
@@ -10,41 +10,54 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+import java.util.ArrayList;
+
 
 /**
  *
  * @author sara
  */
-public class Agenda extends AgendaForm {
+public class Agenda {
     
+    ArrayList<AgendaItem> AgendaItems = new ArrayList<AgendaItem>();
+    
+    public Agenda() {
+        AgendaImport();
+    }
+
     public void AgendaImport() {
-        String fileLocation = "C:\\Users\\sara\\OneDrive\\Documents\\Agenda.txt";
-        File file = new File(fileLocation);
-        
+                
         try {
+            
+            String fileLocation = "C:\\Users\\sara\\OneDrive\\Documents\\Agenda.txt";
+            File file = new File(fileLocation);
+            if (!file.exists()) {
+                return;
+            }
             BufferedReader br = new BufferedReader(new FileReader(file));
-            DefaultTableModel table = (DefaultTableModel)AgendaTable.getModel();          
             Object[] agendaLines = br.lines().toArray();
-            table.setRowCount(0);
+            AgendaItems.clear();
             
             for(int i = 0; i < agendaLines.length; i++) {
                 String agendaLine = agendaLines[i].toString().trim();
                 String[] agendaItem = agendaLine.split(",");
-                table.addRow(agendaItem);
+                AgendaAdd(agendaItem[0], agendaItem[1], agendaItem[2], Integer.parseInt(agendaItem[3]));
             }
             br.close();
-        } catch (Exception ex) {
-            Logger.getLogger(Agenda.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "C:\\Users\\sara\\OneDrive\\Documents\\Agenda.txt does not exist.");
+        } 
     }
     
     public void AgendaExport() {
         // create new text file if not already created
         // save contents of table to text file
+        AgendaItem AgendaItem;
         try {
             File file = new File("C:\\Users\\sara\\OneDrive\\Documents\\Agenda.txt");
             if (!file.exists()) {
@@ -54,81 +67,58 @@ public class Agenda extends AgendaForm {
             FileWriter fw = new FileWriter(file.getAbsoluteFile());
             BufferedWriter bw = new BufferedWriter(fw);
             
-            for (int i = 0; i < AgendaTable.getRowCount(); i++) {
-                for (int j = 0; j < AgendaTable.getColumnCount(); j++) {
-                    bw.write((String)AgendaTable.getModel().getValueAt(i,j) + ",");
-                }
+            for (int i = 0; i < AgendaItems.size(); i++) {
+                AgendaItem = AgendaItems.get(i);
+                
+                bw.write(AgendaItem.AgendaCourse + ",");
+                bw.write(AgendaItem.AgendaType + ",");
+                bw.write(AgendaItem.AgendaDate + ",");
+                bw.write(AgendaItem.AgendaGrade + ",");
+                
                 bw.write(" \n");
             }
             bw.close();
             fw.close();
             JOptionPane.showMessageDialog(null, "Data has been saved");
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "C:\\Users\\sara\\OneDrive\\Documents\\Agenda.txt could not be created.");
+        } 
     }
     
-    public void AgendaAdd() {
-        // get user input
-        String agendaCourse = JOptionPane.showInputDialog("Enter the course");
-        String agendaType = JOptionPane.showInputDialog("Enter the type of assessment due");
-        String agendaDate = JOptionPane.showInputDialog("Enter the due date");
-        String agendaGrade = JOptionPane.showInputDialog("Enter the grade if recieved");
-        
-        // put data into new table row
-        Object agendaItem[] = new Object[4];
-        agendaItem[0] = agendaCourse;
-        agendaItem[1] = agendaType;
-        agendaItem[2] = agendaDate;
-        agendaItem[3] = agendaGrade;
-        
-        DefaultTableModel table = (DefaultTableModel)AgendaTable.getModel();
-        table.addRow(agendaItem);
-    }
-    
-    public void AgendaUpdate() {
-        DefaultTableModel table = (DefaultTableModel)AgendaTable.getModel();
-        int selectedRow = AgendaTable.getSelectedRow();
-        int selectedCol = AgendaTable.getSelectedColumn();
-        String message = null;
-        
-        // replace data in selected table cell if any changes are made
-        try {
-            String agendaItem = table.getValueAt(selectedRow, selectedCol).toString();
-            switch (selectedCol) {
-                case 0:
-                    message = "Enter any changes to the course";
-                    break;
-                case 1:
-                    message = "Enter any changes to the type of assessment due";
-                    break;
-                case 2:
-                    message = "Enter any changes to the date";
-                    break;
-                case 3:
-                    message = "Enter any changes to the grade";
-                default:
-                    break;
-            }
-                    
-            String newAgendaCourse = JOptionPane.showInputDialog(null, message, agendaItem);
-            table.setValueAt(newAgendaCourse, selectedRow, selectedCol);
+    public void AgendaAdd(String AgendaCourse, String AgendaType, String AgendaDate, int AgendaGrade) {
+        AgendaItem AgendaItem = new AgendaItem();
+        AgendaItem.AgendaCourse = AgendaCourse;
+        AgendaItem.AgendaType = AgendaType;
+        AgendaItem.AgendaDate = AgendaDate;
+        AgendaItem.AgendaGrade = AgendaGrade;
 
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "No cell selected");
-        }
+        AgendaItems.add(AgendaItem);
     }
     
-    public void AgendaDelete() {
-        DefaultTableModel table = (DefaultTableModel) AgendaTable.getModel();
+    public int AgendaItems() {
+        return AgendaItems.size();
+    }
+    
+    public AgendaItem AgendaGet(int itemNumber) {
+    // needs to be bulletproofed
+        return AgendaItems.get(itemNumber);
+    }
+    
+    public void AgendaUpdate(int itemNumber, String AgendaCourse, String AgendaType, String AgendaDate, int AgendaGrade) {
+        AgendaItem AgendaItem = new AgendaItem();
+        AgendaItem.AgendaCourse = AgendaCourse;
+        AgendaItem.AgendaType = AgendaType;
+        AgendaItem.AgendaDate = AgendaDate;
+        AgendaItem.AgendaGrade = AgendaGrade;
+
+        AgendaItems.set(itemNumber, AgendaItem);
         
-        // delete selected row
-        try {
-            int selectedRowIndex = AgendaTable.getSelectedRow();
-            table.removeRow(selectedRowIndex);
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "No row selected");
-        }
+    }
+
+    public void AgendaDelete(int itemNumber) {
+        
+        AgendaItems.remove(itemNumber);
+        
     }
     
 }
